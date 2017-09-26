5c44e1fbd5a01cc60d8d668a84fa926d272a50df
jfreiche <jul.freiche@gmail.com>
2017-09-26 10:54:48 +0200

    First commit

---
+++ src/main/java/cpe/App.java
@@ -0,0 +1,18 @@
+package cpe;
+
+import org.joda.time.DateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        DateTime now = new DateTime();
+        System.out.println(now);
+    }
+}
